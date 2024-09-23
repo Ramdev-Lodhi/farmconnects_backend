1a6ba2b46eb7cc3fef9beb0edc93d4f87e814412
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2024-09-23 22:12:04 +0530

    feat: Source map support setup

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,6 +4,10 @@ import util from 'util'
 import config from '../config/config'
 import { EApplicationEnvironment } from '../constant/application'
 import path from 'path'
+import * as sourceMapSupport from 'source-map-support'
+
+//linking trace support
+sourceMapSupport.install()
 
 const consoleLogFormat = format.printf((info) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment

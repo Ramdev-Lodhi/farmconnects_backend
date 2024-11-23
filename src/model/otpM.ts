eb7cd733b7ae49ff5396fa93caffd7ae3636536c
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2024-11-23 12:02:29 +0530

    feat: Otp Model updatep

--- src/model/otpM.ts
+++ src/model/otpM.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const otpSchema = new mongoose.Schema({
     phone: { type: String, required: true },
     otp: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: '1m' }
+    createdAt: { type: Date, default: Date.now, expires: '60' }
 })
 
 export const OtpModel = mongoose.model('Otp', otpSchema)

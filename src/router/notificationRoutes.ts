2680ffb9511cfd0cde52b5f21ad714628ecdf9fa
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2024-11-23 18:37:19 +0530

    feat: Update Notification router

--- src/router/notificationRoutes.ts
+++ src/router/notificationRoutes.ts
@@ -4,4 +4,5 @@ import NotificationController from '../controller/notificationController'
 const router = express.Router()
 
 router.route('/send-notification').post(NotificationController.sendNotification)
+router.route('/send-contact-notification').post(NotificationController.sendContactNotification)
 export default router

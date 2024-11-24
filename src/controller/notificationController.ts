7c4f19547d73d4306c9d287aa1ad3cf4f7c99190
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2024-11-24 16:15:43 +0530

    feat: Notification Update

--- src/controller/notificationController.ts
+++ src/controller/notificationController.ts
@@ -6,6 +6,7 @@ import httpResponse from '../util/httpResponse'
 import responseMessage from '../constant/responseMessage'
 import { Register } from '../model/UserM'
 import httpError from '../util/httpError'
+import logger from '../util/logger'
 interface FirebaseMessageResult {
     error?: {
         code: string
@@ -67,6 +68,7 @@ export default {
         notificationResults.forEach((result, index) => {
             if (result.status === 'rejected') {
                 const firebaseError = result.reason as FirebaseMessageResult
+                logger.error(`Error for token: ${tokens[index]} - ${JSON.stringify(result.reason)}`)
                 if (
                     firebaseError.error &&
                     (firebaseError.error.code === 'messaging/registration-token-not-registered' ||
@@ -74,6 +76,8 @@ export default {
                 ) {
                     invalidTokens.push(tokens[index])
                 }
+            } else {
+                logger.info(`Notification sent successfully to token: ${tokens[index]}`)
             }
         })
 
@@ -81,6 +85,17 @@ export default {
             await Register.updateMany({ deviceTokens: { $in: invalidTokens } }, { $pull: { deviceTokens: { $in: invalidTokens } } })
         }
 
-        httpResponse(req, res, 200, responseMessage.NOTIFICATION, { success: true, invalidTokens })
+        const hasSuccess = notificationResults.some((result) => result.status === 'fulfilled')
+        if (!hasSuccess && invalidTokens.length > 0) {
+            return httpResponse(req, res, 400, 'All device tokens are invalid. Notification not sent.', {
+                success: false,
+                invalidTokens
+            })
+        }
+
+        httpResponse(req, res, 200, responseMessage.NOTIFICATION, {
+            success: true,
+            invalidTokens
+        })
     })
 }

44987c282634885733fc8d595504d91c1e5647bc
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2024-11-25 16:56:49 +0530

    feat: Update Rent model

--- src/model/RentM.ts
+++ src/model/RentM.ts
@@ -1,77 +1,44 @@
 import mongoose, { Schema } from 'mongoose'
 
-const rentSchema = new mongoose.Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'userM'
-    },
-    userInfo: {
-        name: {
-            type: String,
-            trim: true
+const rentSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'userM'
         },
-        mobile: {
-            type: String
+        userInfo: {
+            name: { type: String, trim: true },
+            mobile: { type: String },
+            email: { type: String, trim: true, lowercase: true }
         },
-        email: {
-            type: String,
-            trim: true,
-            lowercase: true
-        }
-    },
-    serviceType: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    price: {
-        type: String,
-        require: true,
-        trim: true
-    },
-    rentedStatus: {
-        type: Boolean,
-        default: false
-    },
-    state: {
-        type: String
-    },
-    district: {
-        type: String
-    },
-    sub_district: {
-        type: String
-    },
-    village: {
-        type: String
-    },
-    pincode: {
-        type: String
-    },
-    image: {
-        type: String
-    },
-    serviceRequests: [
-        {
-            requestedBy: {
-                type: Schema.Types.ObjectId,
-                ref: 'UserM'
-            },
-            requestStatus: {
-                type: String,
-                enum: ['Pending', 'Approved', 'Rejected'],
-                default: 'Pending'
+        serviceType: { type: String, required: true, trim: true },
+        price: { type: String, required: true, trim: true },
+        rentedStatus: { type: Boolean, default: true },
+        state: { type: String },
+        district: { type: String },
+        sub_district: { type: String },
+        village: { type: String },
+        pincode: { type: String },
+        image: { type: String },
+        serviceRequests: [
+            {
+                requestedBy: { type: Schema.Types.ObjectId, ref: 'UserM' },
+                name: { type: String, trim: true },
+                mobile: { type: String },
+                location: { type: String, trim: true },
+                requestStatus: {
+                    type: String,
+                    enum: ['Pending', 'Approved', 'Rejected'],
+                    default: 'Pending'
+                },
+                requestedFrom: { type: Date, required: true },
+                requestedTo: { type: Date, required: true }
             }
-            // requestedFrom: {
-            //     type: Date,
-            //     required: true
-            // },
-            // requestedTo: {
-            //     type: Date,
-            //     required: true
-            // }
-        }
-    ]
-})
+        ]
+    },
+    {
+        timestamps: true
+    }
+)
 const Rent = mongoose.model('Rent', rentSchema)
 export { Rent }
